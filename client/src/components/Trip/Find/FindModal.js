fb5f0abf6bff44999c12b6bab90c66fc5d7eb876
Abigail Williams <absarah@boston.cs.colostate.edu>
2021-09-20 15:35:38 -0600

    Update client structure with added Find File.

---
+++ client/src/components/Trip/Find/FindModal.js
@@ -0,0 +1,8 @@
+// Renders input box in the Modal (keeps it contained external to the main page)
+// Renders result (potentially use list component)
+    // Progressive Disclosure
+// Getting data for Planner
+    // callback function to get parent
+    // Button - use ReactStract 
+    // Good for longevity
+// LOOK into "usestate" managing places 
--- client/src/components/Trip/Planner.js
+++ client/src/components/Trip/Planner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Col, Container, Row } from 'reactstrap';
 import Map from './Map/Map';
 import Itinerary from './Itinerary/Itinerary';
+import Find from './Find/FindModal';
 import { usePlaces } from '../../hooks/usePlaces';
 
 export default function Planner() {
@@ -9,6 +10,11 @@ export default function Planner() {
 
     return (
         <Container>
+            <Row>
+            <Col sm={12} md={{ size: 10, offset: 1 }}>
+                {props.children} <Map places={places} selectedIndex={selectedIndex} placeActions={placeActions} />
+            </Col>
+            </Row>
             <Section>
                 <Map places={places} selectedIndex={selectedIndex} placeActions={placeActions} />
             </Section>
