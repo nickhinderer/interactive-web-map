9d97a68bcca73d904855e6dc4dad0f39b3be2051
Jacquelyn Hyatt <jachyatt1@gmail.com>
2021-09-21 09:45:03 -0600

    Update Image link

--- client/src/components/Trip/Find/FindModal.js
+++ client/src/components/Trip/Find/FindModal.js
@@ -13,7 +13,7 @@ LOOK INTO "useState" as a form of managing places
 
 import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import { ReactComponent as Search } from './static/images/geo.svg'
+import { ReactComponent as SearchImage } from '../client/src/static/images/geo.svg'
 
 const FindModal = () => {
     const [modal, setModal] = useState(false);
@@ -21,7 +21,7 @@ const FindModal = () => {
 
     return (
         <div>
-            <Button onClick={toggle}><Search />Search</Button>
+            <Button onClick={toggle}><SearchImage />Search</Button>
             <Modal isOpen={modal} toggle={toggle}>
                 <ModalHeader>Search</ModalHeader>
                 <ModalBody>Search Bar Goes Here</ModalBody>
