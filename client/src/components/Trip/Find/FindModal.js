352ebcdf0757d5cea61937f797bd58ccc01e8564
kaiyanzzz <kaiyanzeng30@gmail.com>
2021-09-28 16:53:31 -0600

    update FindModal for show list

--- client/src/components/Trip/Find/FindModal.js
+++ client/src/components/Trip/Find/FindModal.js
@@ -6,33 +6,11 @@ import List from './List';
 
 const FindModal = () => {
     const [modal, setModal] = useState(false);
-    const [inputLinkClicked, setInputLinkClicked] = useState(false);
     const toggle = () => setModal(!modal);
     const [display, setDisplay] = useState(false);
-    const [Address, setAddress] = useState("Address");
-    const [State, setState] = useState("State");
-    const [City, setCity] = useState("City");
-    let match = [{ id: 'Address', value: Address },
-    { id: 'City', value: City },
-    { id: 'State', value: State }];
-
-
-   
-
-
-    function updatematch() {
-        match = ([{ id: 'Address', value: Address },
-        { id: 'City', value: City },
-        { id: 'State', value: State }]);
-        //this is for test the storage of input value;
-        console.log(match);
-        setDisplay(!display);
-        
-       
-    }
+    const [match, setMatch] = useState("Address");
+ 
     
-    
-
     return (
 
         <div className="searchButton">
@@ -45,25 +23,14 @@ const FindModal = () => {
                             <InputGroupText color="green" > Address</InputGroupText>
 
                         </InputGroupAddon>
-                        <Input placeholder={Address} onChange={e => setAddress(e.target.value)} />
+                        <Input placeholder={match} onChange={e => setMatch(e.target.value)} />
                     </InputGroup>
 
-                    <InputGroup>
-                        <InputGroupAddon addonType="append">
-                            <InputGroupText>City</InputGroupText>
+                    
 
-                        </InputGroupAddon>
-                        <Input placeholder={City} onChange={e => setCity(e.target.value)} />
-
-                        <InputGroupAddon addonType="append">
-                            <InputGroupText>State</InputGroupText>
-                        </InputGroupAddon>
-                        <Input placeholder={State} onChange={e => {setState(e.target.value);updatematch();}} />
-                    </InputGroup>
-
-                    <Button color="primary" id="button-addon1" outline type="button" onClick={() =>setInputLinkClicked(true) }>Search </Button>
+                    <Button color="primary" id="button-addon1" outline type="button" onClick={() =>setDisplay(!display)}>Search </Button>
                     <br/>
-                    {inputLinkClicked?<List match={match}/> :<div></div>}
+                    {display? <List match={match}/> :<div></div>}
                     
 
                 </ModalBody>
