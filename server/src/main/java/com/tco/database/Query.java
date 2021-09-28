80bb0574879cf3dfcdc95aac82d113c6ef144134
Nicholas Hinderer <nxhinde@rams.colostate.edu>
2021-09-28 15:45:03 -0600

    -modified getSQL function to search for count directly and match name

--- server/src/main/java/com/tco/database/Query.java
+++ server/src/main/java/com/tco/database/Query.java
@@ -27,16 +27,18 @@ public class Query {
     private static String getSQL(String match, Integer limit) {
         if(limit == -1) {
             return
-                    "SELECT " + "name, lattitude, longitude, altitude, type, country, home_link "
+                    "SELECT "
+                            + "COUNT(*)"
                             + "FROM world WHERE name LIKE '%" + match
                             + "%';";
         } else {
 
             return
-                    "SELECT " + "name, lattitude, longitude, altitude, type, country, home_link, "//
-                            + "FROM world WHERE name like '" + match
+                    "SELECT "
+                            + "name, latitude, longitude, altitude, type, country, home_link, "//
+                            + "FROM world WHERE name like '%" + match
                             + "%' LIMIT "  + limit.toString()  + ";";
-        }//add join table too
+        }//add join table too. and type and where
 
     }
 }
