d41e4c42c0819b0b624f275e73826f9ca057dbce
Nicholas Hinderer <nxhinde@rams.colostate.edu>
2021-09-28 15:44:14 -0600

    -modified Query.java constructor -added Query method

--- server/src/main/java/com/tco/database/Query.java
+++ server/src/main/java/com/tco/database/Query.java
@@ -6,16 +6,22 @@ import com.tco.misc.BadRequestException;
 public class Query {
     private String match;
     private Integer limit;
+    private Database database;
 
-    public Query(String match, Integer limit) {
+    public Query(String match, Integer limit) throws BadRequestException {
         this.match = match;
         this.limit = limit;
+        this.database = new Database();
     }
 
     public Places findMatchingPlaces() throws BadRequestException {
         String sql = getSQL(this.match, this.limit);
-        Database db = new Database();
-        return db.query(sql);
+        return (Places) database.query(sql);
+    }
+
+    public Integer findNumberOfMatches() throws BadRequestException {
+        String sql = getSQL(this.match, this.limit);
+        return (Integer) database.query(sql);
     }
 
     private static String getSQL(String match, Integer limit) {
