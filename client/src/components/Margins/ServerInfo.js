1b7113458d24e3c40dcb71c9be44ecb8d646550f
kaiyanzzz <kaiyanzeng30@gmail.com>
2021-10-05 04:51:51 -0600

    add all teams' server information

---
+++ client/src/components/Margins/ServerInfo.js
@@ -0,0 +1,152 @@
+
+
+export const ServerData = [
+    {
+        name: "T00 DOUBLE AUGHTS",
+        index:1,
+        URL: "https://localhost:31400/"
+    },
+    {
+        name: "T01 TRNG",
+        index:2,
+        URL: "https://localhost:31401/"
+    },
+    {
+        name: "T02 GIT GOOD",
+        index:3,
+        URL: "https://localhost:31402/"
+    },
+    {
+        name: "T03 INDECISIVE INDIVIDUALS",
+        index:4,
+        URL: "https://localhost:31403/"
+    },
+    {
+        name: "T04 GEEK VELOCITY",
+        index:5,
+        URL: "https://localhost:31404/"
+    },
+    {
+        name: "T05 TABSOVERSPACES",
+        index:6,
+        URL: "https://localhost:31405/"
+    },
+    {
+        name: "T06 HERE FOR NOW",
+        index:7,
+        URL: "https://localhost:31406/"
+    },
+    {
+        name: "T07 007",
+        index:8,
+        URL: "https://localhost:31407/"
+    },
+    {
+        name: "T08 AMBIGUOUS AARDVARKS",
+        index:9,
+        URL: "https://localhost:31408/"
+    },
+    {
+        name: "T09 CODING CREW",
+        index:10,
+        URL: "https://localhost:31409/"
+    },
+    {
+        name: "T10 BRUNCH BOYS",
+        index:11,
+        URL: "https://localhost:31410/"
+    },
+    {
+        name: "T11 THE BREWSTERS",
+        index:12,
+        URL: "https://localhost:31411/"
+    },
+    {
+        name: "T12 DODECA-RAMS",
+        index:13,
+        URL: "https://localhost:31412/"
+    },
+    {
+        name: "T13 PENGUINZ",
+        index:14,
+        URL: "https://localhost:31413/"
+    },
+    {
+        name: "T14 THE KAHUNAS",
+        index:15,
+        URL: "https://localhost:31414/"
+    },
+    {
+        name: "T15 GOLDEN POTATOES",
+        index:16,
+        URL: "https://localhost:31415"
+    },
+    {
+        name: "T16 THE SIXTEENS",
+        index:17,
+        URL: "https://localhost:31416/"
+    },
+    {
+        name: "T17 WE PUSH TO MAIN",
+        index:18,
+        URL: "https://localhost:31417/"
+    },
+    {
+        name: "T18 BREAK MAIN",
+        index:0,
+        URL: "https://localhost:31418/"
+    },
+    {
+        name: "T19 THE SPACE BETWEEN",
+        index:19,
+        URL: "https://localhost:31419/"
+    },
+    {
+        name: "T20 SQUINTING IN THE BACK",
+        index:20,
+        URL: "https://localhost:31420/"
+    },
+    {
+        name: "T21 CTRL ALT ELITE",
+        index:21,
+        URL: "https://localhost:31421/"
+    },
+    {
+        name: "T22 2 EAZY",
+        index:22,
+        URL: "https://localhost:31422/"
+    },
+    {
+        name: "T23 STUCK IN BETA",
+        index:23,
+        URL: "https://localhost:31423/"
+    },
+    {
+        name: "T24 24 KARAT",
+        index:24,
+        URL: "https://localhost:31424/"
+    },
+    {
+        name: "T25 BOAT SMARTS",
+        index:25,
+        URL: "https://localhost:31425/"
+    },
+    {
+        name: "T26 HAMILTONIAN CYCLE",
+        index:26,
+        URL: "https://localhost:31426/"
+    },
+    {
+        name: "T27 CODEY CREW",
+        index:27,
+        URL: "https://localhost:31427/"
+    },
+    {
+        name: "T99 TEAM NAME",
+        index:28,
+        URL: "https://localhost:31499/"
+    }
+    
+    
+    
+];
