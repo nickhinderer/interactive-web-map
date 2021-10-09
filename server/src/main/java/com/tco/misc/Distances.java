dfb1836aee4b423a93b887c6db4a94d3434ad8b2
Nick Hinderer <nxhinde@colostate.edu>
2021-10-08 20:21:49 -0600

    added formula for computing central angle

--- server/src/main/java/com/tco/misc/Distances.java
+++ server/src/main/java/com/tco/misc/Distances.java
@@ -1,6 +1,8 @@
 package com.tco.misc;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.lang.Math;
 
 public class Distances {
     private ArrayList<Places> places;
@@ -13,6 +15,15 @@ public class Distances {
         this.distances = new ArrayList<>();
     }
 
+    private Double computeCentralAngle(HashMap<String, Double> coordinates) {
+        double latitude1 = coordinates.get("latitude1");
+        double latitude2 = coordinates.get("latitude2");
+        double longitudeDifference = Math.abs(coordinates.get("longitude1") - coordinates.get("longitude2"));
+        double a = Math.sqrt(Math.pow((Math.cos(latitude2) * Math.sin(longitudeDifference)), 2)
+                + Math.pow(((Math.sin(latitude1) * Math.sin(latitude2)) - (Math.sin(latitude1) * Math.cos(latitude2) * Math.cos(longitudeDifference))), 2));
+        double b = (Math.sin(latitude1) * Math.sin(latitude2)) + (Math.sin(latitude1) * Math.cos(latitude2) * Math.cos(longitudeDifference));
+        return Math.atan2(a, b);
+    }
 
     public ArrayList<Integer> computeDistances() {
         return new ArrayList<>();
