19bd6acb9a17b3a5adf6f10d362858b58ce67e36
Jacki Hyatt <jachyatt1@gmail.com>
2021-10-11 10:00:40 -0600

    Test basic components

--- client/test/components/Trip/Find.test.js
+++ client/test/components/Trip/Find.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import user from '@testing-library/user-event';
-import { beforeEach, describe, expect, it } from '@jest/globals';
+import { beforeEach, describe, expect, it, test } from '@jest/globals';
 import FindModal from '../../../src/components/Trip/Find/FindModal';
 
 describe('FindModal', () =>{
@@ -9,7 +9,12 @@ describe('FindModal', () =>{
         render(<FindModal/>)
     });
 
-    it('contains a search button', () =>{
-
+    it('contains a search button image', () =>{
+        expect(screen.getByRole('img', { name: /search/i })).toBeDefined()
     });
+
+    it('contains a search button', () =>{
+        expect(screen.getByRole('button', { toggle: true })).toBeDefined()
+    });
+
 });
