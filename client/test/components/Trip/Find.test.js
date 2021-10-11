433642535444f3af090ef3d4ada0923059778d57
Jacki Hyatt <jachyatt1@gmail.com>
2021-10-11 10:31:52 -0600

    Start onClick testing

--- client/test/components/Trip/Find.test.js
+++ client/test/components/Trip/Find.test.js
@@ -10,11 +10,19 @@ describe('FindModal', () =>{
     });
 
     it('contains a search button image', () =>{
-        expect(screen.getByRole('img', { name: /search/i })).toBeDefined()
+        expect(screen.getByRole('img', { name: /search/i })).toBeDefined();
     });
 
     it('contains a search button', () =>{
-        expect(screen.getByRole('button', { toggle: true })).toBeDefined()
+        expect(screen.getByRole('button', { toggle: false })).toBeDefined();
     });
+
+    it('toggles modal when clicked', () =>{
+        const searchButton = screen.getByRole('img', { name: /search/i });
+        //Add correct testing for modal here
+
+    });
+
+    //Add testing for input box here
 
 });
