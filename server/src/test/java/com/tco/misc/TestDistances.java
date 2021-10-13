d8783641b8f796759d7dbed1730fc2e10e4c883f
Nick Hinderer <nxhinde@colostate.edu>
2021-10-13 12:04:08 -0600

    updated configuration method for TestDistances

--- server/src/test/java/com/tco/misc/TestDistances.java
+++ server/src/test/java/com/tco/misc/TestDistances.java
@@ -1,6 +1,7 @@
 package com.tco.misc;
 
 import com.tco.misc.Distances;
+import com.tco.misc.Places;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,8 +11,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TestDistances {
+
     @BeforeEach
     public void createConfigurationForTestCases() {
-
+        Distances conf = new Distances(new Places(), 0);
+        conf.computeDistances();
     }
 }
