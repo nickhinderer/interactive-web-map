29928945d5129a86f2f84db7444eaea93bdae45a
kaiyan zeng <kaiyanzeng30@gmail.com>
2021-10-12 20:02:11 -0600

    add list.test.js

--- client/test/components/Trip/List.test.js
+++ client/test/components/Trip/List.test.js
@@ -5,14 +5,34 @@ import { beforeEach, describe, expect, it, test } from '@jest/globals';
 import List from '../../../src/components/Trip/Find/List';
 
 describe('List', () =>{
-   
+   const processServerDistancesSuccess = jest.fn()
+   processServerDistancesSuccess=>Promise.resolve({data:null});
     beforeEach(() =>{
         render(<List/>)
     });
 
-    it('renders an Add button', () => {
-        expect(screen.findByText(/Add/i)).toBeDefined();
+    it('renders an Add button', async ()=> {
+        expect(screen.findByRole('button', { name: /Add/i })).toBeDefined();
+        
     });
+
+    //it('contains a Add button image', () =>{
+        //expect(screen.getByRole('img', { name: /add/i })).toBeDefined();
+    //});
+
+    it('contain expected information', async ()=> {
+        screen.findByText(/Name:/i);
+        screen.findByText(/Country:/i);
+        screen.findByText(/Latitude:/i);
+        screen.findByText(/Longitude:/i); 
+    });
+             
     
 
+    it('contain not result found', async () => {
+        screen.findByText(/No results found /i);
+             
+    });
+
+  
 });
