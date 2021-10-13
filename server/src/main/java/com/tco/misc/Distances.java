995c3759c6ae7db63a2406965d5c41632f37b6f4
Nick Hinderer <nxhinde@colostate.edu>
2021-10-13 17:55:02 -0600

    fixed logic for computing distances

--- server/src/main/java/com/tco/misc/Distances.java
+++ server/src/main/java/com/tco/misc/Distances.java
@@ -36,17 +36,15 @@ public class Distances {
 
     public ArrayList<Integer> computeDistances() {
         int n = this.places.size();
-        for(int i = 1; i < n; i++) { //outer loop to measure difference in index of the two points you're comparing
-            for(int j = 0; j+i < n; j++) { //inner loop to compare each pair of points starting at a distance of 1 between each point, outer loop then increments until the first and last element are compared
-                HashMap<String, Double> coordinates= new HashMap<>();
-                coordinates.put("latitude1", Double.valueOf(places.get(j).get("latitude")));
-                coordinates.put("latitude2", Double.valueOf(places.get(j+i).get("latitude")));
-                coordinates.put("longitude1", Double.valueOf(places.get(j).get("longitude")));
-                coordinates.put("longitude2", Double.valueOf(places.get(j+i).get("longitude")));
-                double centralAngle = computeCentralAngle(coordinates);
-                Double distance = earthRadius * centralAngle;
-                distances.add(distance.intValue());
-            }
+        for(int i = 1; i < n; i++) {
+            HashMap<String, Double> coordinates= new HashMap<>();
+            coordinates.put("latitude1", Double.valueOf(places.get(0).get("latitude")));
+            coordinates.put("latitude2", Double.valueOf(places.get(i).get("latitude")));
+            coordinates.put("longitude1", Double.valueOf(places.get(0).get("longitude")));
+            coordinates.put("longitude2", Double.valueOf(places.get(i).get("longitude")));
+            double centralAngle = computeCentralAngle(coordinates);
+            Double distance = earthRadius * centralAngle;
+            distances.add(distance.intValue());
         }
         return distances;
     }
