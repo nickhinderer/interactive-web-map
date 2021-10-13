76e1207744dd189ef49b01d3fa8302defa9d22f6
Nick Hinderer <nxhinde@colostate.edu>
2021-10-13 11:56:06 -0600

    created configuration method declaration for TestDistances

--- server/src/test/java/com/tco/misc/TestDistances.java
+++ server/src/test/java/com/tco/misc/TestDistances.java
@@ -1,7 +1,17 @@
 package com.tco.misc;
 
+import com.tco.misc.Distances;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TestDistances {
+    @BeforeEach
+    public void createConfigurationForTestCases() {
+
+    }
 }
