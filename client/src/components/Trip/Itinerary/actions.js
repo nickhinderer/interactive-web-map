0c46bf71048af955161bb0e3b44c5e7624ead23d
absarah <abbywilliams789@gmail.com>
2021-10-18 16:44:48 -0600

    Add readFile() call from actions.js in itinerary hamburger

--- client/src/components/Trip/Itinerary/actions.js
+++ client/src/components/Trip/Itinerary/actions.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Button, ButtonGroup, DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown } from 'reactstrap';
+import { Input, ButtonGroup, DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown } from 'reactstrap';
 import { BiDotsVerticalRounded } from 'react-icons/bi';
 import { FaHome, FaTrash, FaTrashAlt } from 'react-icons/fa';
 import { DEFAULT_STARTING_PLACE } from '../../../utils/constants';
 
+const FILE_FORMATS = ".json, .csv, application/json, text/csv";
+
 export function ItineraryActionsDropdown(props) {
+    
+    function handleFileUpload(event) {
+        const fileName = event.target.files[0].name;
+        const fileObject = event.target.files[0];
+        props.placeActions.readFile(fileName, fileObject)
+    }
+
     return (
         <ActionsDropdown {...props}>
             <DropdownItem onClick={() => props.placeActions.moveToHome()} data-testid='home-button'>
@@ -13,7 +22,9 @@ export function ItineraryActionsDropdown(props) {
             <DropdownItem onClick={() => props.placeActions.removeAll()} data-testid='delete-all-button'>
                 <FaTrashAlt />
             </DropdownItem>
-            <Button size="sm" > Load File </Button>
+            <Input type="file" accept={FILE_FORMATS} onChange={handleFileUpload} data-testid='load-file-button'/>
+            {/*<Button size="sm" onClick={() => props.placeActions.FileLoaderApp()} > Load File </Button> 
+            <Button size="sm" > Load File </Button> */}
         </ActionsDropdown>
     );
 }
@@ -30,12 +41,12 @@ export function PlaceActionsDropdown(props) {
 
 function ActionsDropdown(props) {
     return (
-        <UncontrolledDropdown direction="left">
-            <DropdownToggle tag="div" data-testid={`row-toggle-${props.index}`}>
+        <UncontrolledDropdown direction="left" >
+            <DropdownToggle tag="div" data-testid={`row-toggle-${props.index}`} >
                 <BiDotsVerticalRounded size="1.5em" />
-            </DropdownToggle>
-            <DropdownMenu>
-                <ButtonGroup>
+            </DropdownToggle >
+            <DropdownMenu >
+                <ButtonGroup >
                     {props.children}
                 </ButtonGroup>
             </DropdownMenu>
