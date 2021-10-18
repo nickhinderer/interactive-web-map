ecd44c59da98fc9023606f08e54e1e13010eb90b
Nick Hinderer <nxhinde@colostate.edu>
2021-10-18 09:06:10 -0600

    added query test class

---
+++ server/src/test/java/com/tco/misc/TestQuery.java
@@ -0,0 +1,13 @@
+package com.tco.misc;
+
+import com.tco.database.Query;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestQuery {
+
+}
