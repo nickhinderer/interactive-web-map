871b1a66bd938b63154135d648c14b6a6c5f8081
Jacki Hyatt <jachyatt1@gmail.com>
2021-10-20 12:36:28 -0600

    Move returnTotal to inner function

--- client/src/components/Distances/TotalDistance.js
+++ client/src/components/Distances/TotalDistance.js
@@ -11,45 +11,44 @@ export default function TotalDistance(props){
 }
 
 function Sum(props){
-    let sum = 0;
-    for(let i = 0; i < props.length; ++i){
-        sum += props[i];
+        let sum = 0;
+        for(let i = 0; i < props.length; ++i){
+            sum += props[i];
+        }
+
+        return sum;
     }
 
-    return sum;
-}
-
-function Row(props){
+    function Row(props){
     //need to pass distances to Sum() then show here
-    return (
-        <thead>
-            <tr>
-                <td>Trip Distance:</td>
-                <td>{0} Miles</td>
-            </tr>
-        </thead>
-    );
-}
-
-async function sendDistancesRequest(data) {
-    const[distances, setDistances] = useState([]);
-    const[err, setErr] = useState(false);
-
-    const serverUrl = getOriginalServerUrl();
-    const distancesResponse = await sendAPIRequest({ requestType: "distances", places: data, earthRadius: 3959 }, serverUrl);
-    if (distancesResponse!=null) {
-        distancesResponse.distances.length==0? setErr(true):setErr(false),setDistances(distancesResponse.distances); 
+        return (
+            <thead>
+                <tr>
+                    <td>Trip Distance:</td>
+                    <td>{0} Miles</td>
+                </tr>
+            </thead>
+        );
     }
 
-    return(
-        distances
-    );
-}
+    async function sendDistancesRequest(data) {
+        const[distances, setDistances] = useState([]);
+        const[err, setErr] = useState(false);
 
-function returnTotal(props){
-    return (
-        //err ? <Alert color="primary"> Choose at least two places to calculate distances!</Alert>:
-        //figure out how to grab err from distance request
-        <Alert>{Sum(props)} Miles</Alert>
-    );
-}
+        const serverUrl = getOriginalServerUrl();
+        const distancesResponse = await sendAPIRequest({ requestType: "distances", places: data, earthRadius: 3959 }, serverUrl);
+        if (distancesResponse!=null) {
+            distancesResponse.distances.length==0? setErr(true):setErr(false),setDistances(distancesResponse.distances); 
+        }
+
+        function returnTotal(props){
+            return (
+                err ? <Alert color="primary"> Choose at least two places to calculate distances!</Alert>:<Alert>{Sum(props)} Miles</Alert>
+                //figure out how to grab err from distance request
+            );
+        }
+        
+        return(
+            returnTotal(distances)
+        );
+    }
