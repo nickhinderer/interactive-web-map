557736aefed62e6085ce233acd535221a2bfc515
Jacki Hyatt <jachyatt1@gmail.com>
2021-10-21 12:33:50 -0600

    Add test for sum

--- client/test/components/Distances/TotalDistance.test.js
+++ client/test/components/Distances/TotalDistance.test.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { beforeEach, describe, expect, it } from '@jest/globals';
-import TotalDistance from '../../../src/components/Distances/TotalDistance';
+import TotalDistance, { Row, Sum, returnTotal } from '../../../src/components/Distances/TotalDistance';
 
 describe('TotalDistance', () =>{
     beforeEach(() => {
-        //render(<TotalDistance/>)
+        render(<TotalDistance distances={[1,2,3]} err={true}/>)
         //Fix TotalDistance class
     });
 
-    it('test', () =>{
+    it('sum returns correct total', () =>{
         //Add tests later
+        const distances = [1, 2, -1];
+        const total = Sum(distances);
+        expect(total).toEqual(2);
     });
+
 });
