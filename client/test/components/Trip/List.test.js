611682d505a57d1b06a8dee77303d705929efbf4
kaiyan zeng <kaiyanzeng30@gmail.com>
2021-10-21 00:54:17 -0600

    update list.test

--- client/test/components/Trip/List.test.js
+++ client/test/components/Trip/List.test.js
@@ -1,29 +1,34 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen,act,fireEvent,waitFor } from '@testing-library/react';
 import user from '@testing-library/user-event';
 import { beforeEach, describe, expect, it, test } from '@jest/globals';
 import List from '../../../src/components/Trip/Find/List';
-
+import { VALID_FIND_RESPONSE } from '../../sharedMocks';
+let addButton;
 describe('List', () =>{
    
     beforeEach(() =>{
+        fetch.resetMocks();
+        fetch.mockResponse(VALID_FIND_RESPONSE);
         render(<List/>)
     });
 
     //still need a fake request to test line 16-25
 
     it('renders an Add button', async ()=> {
-        expect(screen.findByRole('button', { name: /Add/i })).toBeDefined();
+        addButton = screen.findByRole('button', { name: /Add/i });
+        await waitFor(() => {
+            expect(screen.findByText(/Name:/i));
+            expect(screen.findByText(/Country:/i));
+            expect(screen.findByText(/Latitude:/i));
+            expect(screen.findByText(/Latitude:/i));
+             
+         });
         
     });
 
 
-    it('contain expected information', async ()=> {
-        screen.findByText(/Name:/i);
-        screen.findByText(/Country:/i);
-        screen.findByText(/Latitude:/i);
-        screen.findByText(/Longitude:/i); 
-    });
+   
              
     
 
