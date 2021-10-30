445b9f137a7ad48dfde554c29d793e33720e5850
Nick Hinderer <nxhinde@colostate.edu>
2021-10-30 17:18:57 -0600

    added TourRequest members

--- server/src/main/java/com/tco/requests/TourRequest.java
+++ server/src/main/java/com/tco/requests/TourRequest.java
@@ -1,11 +1,17 @@
 package com.tco.requests;
 
 import com.tco.misc.BadRequestException;
+import com.tco.misc.Places;
 
 public class TourRequest extends Request {
 
+    private Double earthRadius;
+    private Double response;
+    private Places places;
+
     @Override
     public void buildResponse() throws BadRequestException {
+        this.requestType = "tour";
 
     }
 }
