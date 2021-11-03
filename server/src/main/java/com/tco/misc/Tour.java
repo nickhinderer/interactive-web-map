fe38217d0cc9f937d4f8707cb9a14b8a9a995d7c
Nick Hinderer <nxhinde@colostate.edu>
2021-11-03 08:26:26 -0600

    added Tour constructor

--- server/src/main/java/com/tco/misc/Tour.java
+++ server/src/main/java/com/tco/misc/Tour.java
@@ -4,4 +4,11 @@ public class Tour {
     private Places places;
     private double earthRadius;
     private Double response;
+
+    public Tour(Places places, double earthRadius, Double response) {
+        this.places = places;
+        this.earthRadius = earthRadius;
+        this.response = response;
+    }
+
 }
