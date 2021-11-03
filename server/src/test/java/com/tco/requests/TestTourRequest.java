1f3457f60adbb489a71898706e4054902dfbd14b
Nick Hinderer <nxhinde@colostate.edu>
2021-11-03 07:57:23 -0600

    updated createConfigurationForTestCases method in TestTourRequest.java

--- server/src/test/java/com/tco/requests/TestTourRequest.java
+++ server/src/test/java/com/tco/requests/TestTourRequest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 
 public class TestTourRequest {
 
+    private TourRequest tour;
+
     @BeforeEach
     public void createConfigurationForTestCases() {
-
+        tour = new TourRequest();
     }
 }
