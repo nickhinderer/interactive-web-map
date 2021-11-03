f0c3b0277b38ced341e39ff337524d58bd8985f7
Nick Hinderer <nxhinde@colostate.edu>
2021-11-03 08:40:01 -0600

    added createConfigurationForTestCases to TestTour

--- server/src/test/java/com/tco/misc/TestTour.java
+++ server/src/test/java/com/tco/misc/TestTour.java
@@ -1,5 +1,13 @@
 package com.tco.misc;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class TestTour {
 
+    Tour tour;
+
+    @BeforeEach
+    public void createConfigurationForTestCases() {
+        tour = new Tour();
+    }
 }
