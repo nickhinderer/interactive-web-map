6809b50a8beba4611c1b48da03e03d3f50332472
Nick Hinderer <nxhinde@colostate.edu>
2021-11-03 07:59:23 -0600

    added testType to TestTourRequest.java and imported junit assertions

--- server/src/test/java/com/tco/requests/TestTourRequest.java
+++ server/src/test/java/com/tco/requests/TestTourRequest.java
@@ -1,6 +1,9 @@
 package com.tco.requests;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestTourRequest {
 
@@ -9,5 +12,11 @@ public class TestTourRequest {
     @BeforeEach
     public void createConfigurationForTestCases() {
         tour = new TourRequest();
+    }
+
+    @Test
+    @DisplayName("Request type is \"tour\"")
+    public void testType() {
+        assertEquals("tour", tour.getRequestType());
     }
 }
