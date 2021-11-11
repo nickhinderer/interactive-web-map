1a0ec1c6aecc2de0d5e2794bbe470b4fe2d470ec
Jacki Hyatt <jachyatt1@gmail.com>
2021-11-11 07:17:20 -0700

    Add variables to hold info in Person.js

--- client/src/components/About/Person.js
+++ client/src/components/About/Person.js
@@ -2,7 +2,12 @@ import React from 'react';
 import AboutCard from "./AboutCard";
 
 export default function Person(props) {
+    const pic = props.pic;
+    const title = props.name;
+    const subTitle = props.homeTown;
+    const text = props.bio;
+
     return (
-        <AboutCard pic={props.pic} title={props.name} subTitle={props.homeTown} text={props.bio} />
+        <AboutCard pic={pic} title={title} subTitle={homeTown} text={bio} />
     );
 }
