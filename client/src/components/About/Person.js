0fe3d9cd23c091db4113eb017614697c0e4e7841
Jacki Hyatt <jachyatt1@gmail.com>
2021-11-11 07:18:42 -0700

    Fix variables passed

--- client/src/components/About/Person.js
+++ client/src/components/About/Person.js
@@ -4,8 +4,8 @@ import AboutCard from "./AboutCard";
 export default function Person(props) {
     const pic = props.pic;
     const title = props.name;
-    const subTitle = props.homeTown;
-    const text = props.bio;
+    const homeTown = props.homeTown;
+    const bio = props.bio;
 
     return (
         <AboutCard pic={pic} title={title} subTitle={homeTown} text={bio} />
