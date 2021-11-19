8c88adf09284f915b03386929ad7f8992545323a
absarah <abbywilliams789@gmail.com>
2021-11-19 11:54:44 -0700

    Clean up whitespace, omit unecessary imports, and organize

--- client/test/components/Trip/actions.test.js
+++ client/test/components/Trip/actions.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import user from '@testing-library/user-event';
-import { beforeEach, describe, expect, it, jest } from '@jest/globals';
+import { beforeEach, describe, expect, it } from '@jest/globals';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { ItineraryActionsDropdown } from '../../../src/components/Trip/Itinerary/actions.js';
 
 describe('Dropdown', () => {
@@ -20,7 +20,7 @@ describe('Dropdown', () => {
     });
 
     it('Load File Icon click', async () => { 
-        fireEvent.click(await screen.findByTestId('load-trip-icon'));
+        fireEvent.click(await screen.findByTestId('load-trip-icon')); // Use fireEvent or user
         expect(screen.getByTestId('load-trip-icon')).toBeTruthy();
     });
 });
