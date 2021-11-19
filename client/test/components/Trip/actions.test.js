2b2ef9a7ccdbc4b52a01713300fd4765e2b004b1
absarah <abbywilliams789@gmail.com>
2021-11-19 11:22:39 -0700

    Add new test structure for actions.js

---
+++ client/test/components/Trip/actions.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import user from '@testing-library/user-event';
+import { beforeEach, describe, it, jest } from '@jest/globals';
+import { ItineraryActionsDropdown } from '../../../src/components/Trip/Itinerary/actions.js';
+
+describe('Dropdown', () => {
+    beforeEach(() =>{
+        render(<ItineraryActionsDropdown/>)
+    });
+    it('contains a search Dropdown', () =>{
+        //expect(screen.findByRole('Dropdown')).toBeDefined();
+    });
+});
+    
