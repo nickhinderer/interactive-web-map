5c23462ca4d0c6104cd2c024148e01cc64031534
Jacki Hyatt <jachyatt1@gmail.com>
2021-11-19 13:22:00 -0700

    Test button click

--- client/test/components/Trip/List.test.js
+++ client/test/components/Trip/List.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render, screen,act,fireEvent,waitFor } from '@testing-library/react';
+import { render, screen, act, fireEvent, waitFor } from '@testing-library/react';
 import user from '@testing-library/user-event';
-import { beforeEach, describe, expect, it, test } from '@jest/globals';
+import { beforeEach, describe, expect, it, jest, test } from '@jest/globals';
 import List from '../../../src/components/Trip/Find/List';
 import { VALID_FIND_RESPONSE } from '../../sharedMocks';
 let addButton;
@@ -12,15 +12,16 @@ describe('List', () =>{
         fetch.mockResponse(VALID_FIND_RESPONSE);
         render(<List match={"Fort Gordon"} places={[{"name":"Fort Gordon Hqs Helipad Heliport", 
                             "latitude":"33.420398", "longitude":"-82.139602", "country":"United States"}]} 
-                            placeActions={jest.fn()}/>)
+                            placeActions={{append: jest.fn()}}/>)
     });
 
-    // need to test lines 37-38, 44, 69-78
+    // need to test lines 37-38, 44, 78
+
     it('contains a list', () => {
         expect(screen.findByRole('List')).toBeDefined();
     });
 
-    it('renders buttons', async ()=> {
+    it('renders buttons', ()=> {
         expect(screen.findByTestId('Add')).toBeDefined();
     });
 
@@ -31,10 +32,9 @@ describe('List', () =>{
         expect(screen.findByText(/Latitude: -82.139602/i)).toBeDefined();
     });
 
-    it('contain not result found', async () => {
-        expect(screen.findByText(/No results found /i));
-             
+    test('button clicked', async () =>{
+       fireEvent.click(await screen.findByTestId('Add'));
+       expect(screen.getByTestId('Add')).toBeTruthy();
     });
 
-  
 });
