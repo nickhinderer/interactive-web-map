28a6ab8f90c07016d11bec2cfb1b2da648f82f12
absarah <abbywilliams789@gmail.com>
2021-12-09 18:26:01 -0700

    Update FindModal.js to handle progressive disclosure

--- client/src/components/Trip/Find/FindModal.js
+++ client/src/components/Trip/Find/FindModal.js
@@ -1,11 +1,10 @@
-
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input, InputGroup, InputGroupAddon} from 'reactstrap';
 import List from './List';
 import mag from '../../../static/images/search.svg';
 import check from '../../../static/images/check.svg';
 import x from '../../../static/images/x.svg';
-import {GiPerspectiveDiceSixFacesRandom} from "react-icons/gi"
+import {GiPerspectiveDiceSixFacesRandom} from "react-icons/gi";
 
 export default function FindModal(props) {
     const [modal, setModal] = useState(false);
@@ -13,17 +12,23 @@ export default function FindModal(props) {
     const [display, setDisplay] = useState(false);
     const [match, setMatch] = useState("");
 
+    useEffect(() => {
+        setDisplay(false);
+    }, [modal])
+
     return (
 
         <div className="searchButton">
-            <Button className="mx-1" outline-color="secondary" onClick={toggle}><img src={mag} alt='search'/></Button>
+            <Button className="mx-1" outline-color="secondary" onClick={toggle}> <img src={mag} alt='search'/> </Button>
             <Modal isOpen={modal} toggle={toggle}>
                 <ModalHeader>Find Places</ModalHeader>
+
                 <ModalBody>
                 <b>Type</b> and <b>Click Search</b> to add a place to itinerary
-                {FindInput(setDisplay, setMatch)}
+                <FindInput setDisplay={setDisplay} setMatch={setMatch} />
                 {display? <List match={match} places={props.places} placeActions={props.placeActions}/> :<div></div>}
                 </ModalBody>
+
                 <ModalFooter>
                     <Button color="primary" onClick={() => {setDisplay(false); toggle();}}><img src={check}/> Done </Button>
                     <Button color="danger" onClick={toggle}><img src={x}/> Cancel </Button>
@@ -33,14 +38,13 @@ export default function FindModal(props) {
     );
 }
 
-export function FindInput(setDisplay, setMatch){
+export function FindInput(props){
 
     return (
         <InputGroup>
-            <Input placeholder={"Place..."} onChange={e => {setMatch(e.target.value); setDisplay(false);}} />
+            <Input placeholder={"Place..."} onChange={e => {props.setMatch(e.target.value); props.setDisplay(true);}} />
                 <InputGroupAddon addonType="append">
-                    <Button color="primary"  onClick={() =>{setDisplay(true)} }> <GiPerspectiveDiceSixFacesRandom/></Button>
-                    <Button color="primary" id="button-addon1" outline type="button" onClick={() =>{setDisplay(true)} }>Search</Button>
+                    <Button color="primary"  onClick={() =>{props.setDisplay(true)} }> <GiPerspectiveDiceSixFacesRandom/></Button>
                 </InputGroupAddon>
         </InputGroup>
     );
